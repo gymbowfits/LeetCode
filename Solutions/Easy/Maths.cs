656470237edf607075082aa761ca4c5979d9d261
Jim Fitzgerald <j.fitzgerald@inpute.com>
2022-01-27 21:46:21 +0000

    Add LinkedLists, Maths, Others and Trees solutions

---
+++ Solutions/Easy/Maths.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.Easy;
+
+public static class Maths
+{
+    // 326. Power of Three
+    public static bool IsPowerOfThree_326(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+        else if ((n == 0) || (n % 3 != 0))
+        {
+            return false;
+        }
+
+        return IsPowerOfThree_326(n / 3);
+    }
+
+    // 412. Fizz Buzz
+    public static IList<string> FizzBuzz_412(int n)
+    {
+        var result = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            var s = new StringBuilder();
+
+            if (i % 3 == 0)
+            {
+                s.Append("Fizz");
+            }
+
+            if (i % 5 == 0)
+            {
+                s.Append("Buzz");
+            }
+
+            if (s.Length == 0)
+            {
+                s.Append(i);
+            }
+
+            result.Add(s.ToString());
+        }
+
+        return result;
+    }
+}
---
+++ Tests/Easy/Trees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.Easy;
+
+public class Trees
+{
+    [Test]
+    [TestCase(new object[] { 3, 9, 20, null, null, 15, 7 }, 3)]
+    [TestCase(new object[] { 1, null, 2 }, 2)]
+    public void MaxDepth_104(object[] tree, int expectedResult)
+    {
+        var root = (Solutions.Easy.Trees.TreeNode)tree;
+
+        var result = Solutions.Easy.Trees.MaxDepth_104(root);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
