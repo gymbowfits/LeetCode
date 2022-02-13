66dc2f0396f5bc06541d50e32473c87c27157f42
gymbowfits <15697162+gymbowfits@users.noreply.github.com>
2022-02-13 10:17:32 +0000

    Add Sorting and Searching tests (#5)
    Co-authored-by: Jim Fitzgerald <j.fitzgerald@inpute.com>

---
+++ Tests/Easy/SortingSearching.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCode.Tests.Easy;
+
+public class SortingSearching
+{
+    [Test]
+    [TestCase(1, 1)]
+    [TestCase(100, 14)]
+    [TestCase(int.MaxValue, 14)]
+    public void FirstBadVersion_278(int n, int badVersion)
+    {
+        var result = Solutions.Easy.SortingSearching.FirstBadVersion_278(n, badVersion);
+
+        Assert.That(result, Is.EqualTo(badVersion));
+    }
+}
